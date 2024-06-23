7b389ebee79aa58a8e70790627f244dde67bf76e
Tobias <tobias.schweighofer0809@gmail.com>
2024-06-23 18:17:11 +0200

    feature: Added Redirect to Weater GitHub Projekt under .../wetter-sms-hb

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from "./page.module.css";
 import { redirect } from "next/navigation";
 
---
+++ app/wetter-sms-hb/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./page.module.css";
+import { redirect } from "next/navigation";
+
+const page = () => {
+
+    redirect("https://github.com/tobix88/wetter-web-sms-hartberg");
+
+    return (
+        <div className={styles.main}>
+            REDIRECTING...
+        </div>
+    )
+}
+
+export default page
